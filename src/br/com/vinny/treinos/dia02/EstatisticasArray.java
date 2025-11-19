0deb884b07febe953da340ca9db467ddbf433e7a
Ppvinny <vinnyseries@hotmail.com>
2025-11-19 16:22:47 -0300

    subindo correção do professor

---
+++ src/br/com/vinny/treinos/dia02/EstatisticasArray.java
@@ -0,0 +1,40 @@
+package br.com.vinny.treinos.dia02;
+
+import java.util.Scanner;
+
+public class EstatisticasArray {
+
+    Scanner sc = new Scanner(System.in);
+
+    int[] valores;
+
+
+
+}
+//  1) Desafio Java — Manipulação de Arrays
+//  Crie uma classe chamada EstatisticasArray.
+//
+//        Requisitos:
+//  Peça ao usuário quantos números ele vai inserir
+//
+//  Crie um array com esse tamanho
+//
+//  Preencha o array com números digitados pelo usuário
+//
+//  Crie métodos separados para:
+//
+//  encontrar o maior
+//
+//  encontrar o menor
+//
+//  calcular a média
+//
+//  contar quantos números são pares
+//
+//  contar quantos são ímpares
+//
+//  Regras:
+//  Tudo dividido em métodos
+//
+//  Nada de resolver tudo no main
+//
